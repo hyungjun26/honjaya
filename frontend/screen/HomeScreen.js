b9c92bb1edba3840056ccadd5190131f0aa25fd8
김형준 <gudwns026@naver.com>
2020-09-22 20:27:45 +0900

    S03P23A409-30 Add PhotoPick.js feature ImageUpload

--- frontend/screen/HomeScreen.js
+++ frontend/screen/HomeScreen.js
@@ -11,7 +11,10 @@ import * as React from "react";
 export default function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Object Remove" />
+      <Button
+        title="Object Remove"
+        onPress={() => navigation.navigate("Object Remove")}
+      />
     </View>
   );
 }
