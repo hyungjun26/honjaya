add45446a9c038eba043a4e3c662c2721fb8422a
yodg3456 <yodg3456@naver.com>
2020-10-06 01:08:50 +0900

    S03P23A409-60 Add object image upload

--- src/main/java/com/example/demo/controller/WebServercontroller.java
+++ src/main/java/com/example/demo/controller/WebServercontroller.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -11,13 +10,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.stream.Stream;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.example.demo.dto.ImageObject;
+import com.example.demo.dto.ImageUploadResponse;
 import com.example.demo.dto.ImageuploadDto;
 import com.example.demo.service.ImageuploadService;
 
@@ -49,9 +48,9 @@ public class WebServercontroller {
 	static int num;
 	// 파일 업로드 이미지 받기
 	 @RequestMapping(value = "/", method = RequestMethod.POST)
-	    public ResponseEntity  handleFileUpload(@RequestParam("img") MultipartFile file,
-	    		MultipartHttpServletRequest mtfRequest ) throws InterruptedException 
+	    public ResponseEntity<ImageUploadResponse> handleFileUpload(@RequestParam("img") MultipartFile file) throws InterruptedException 
 	 	{
+		 ResponseEntity<ImageUploadResponse> res = null;
 	        try {
 				/*
 				 * List<MultipartFile> fileList = mtfRequest.getFiles("file");
@@ -73,25 +72,22 @@ public class WebServercontroller {
 	            String name = file.getOriginalFilename();
 	            
 	            file.transferTo(fileToSave);
-	            System.out.println("파일이 저장된건가 ");
-	            System.out.println(fileToSave);
 	            //// 파일 업로드 하고 파이썬 실행 폴더에 저장까지는 됨 
 
 	            
 	            
-	            //디비에 저장하기 위한 것...
-	            ImageuploadDto image  = new ImageuploadDto();
-	            image.setNum(num);
-	            image.setName(name);
-	            iService.insertimageupload(image);
+//	            //디비에 저장하기 위한 것...
+//	            ImageuploadDto image  = new ImageuploadDto();
+//	            image.setNum(num);
+//	            image.setName(name);
+//	            iService.insertimageupload(image);
 	            // 디비에 난수랑 파일 이름 저장 완료 
 	            
 	            Runtime rt = Runtime.getRuntime();
 	            
 			 	Process p= null;
 			 	// conda run -n my-python-2-env python --version
-			 	String anaconda = "C:\\ProgramData\\Anaconda3\\condabin\\conda.bat activate edge_env";
-			 	
+			 	String anaconda = "C:\\ProgramData\\Anaconda3\\condabin\\conda.bat activate edge_env";		 	
 			 	String path = "C:\\Users\\multicampus\\s03p23a409\\yolact\\eval.py";
 			 	String inputPath = "C:\\Users\\multicampus\\s03p23a409\\yolact\\" + "images\\input\\" + filename;
 			 	String outputPath = "C:\\Users\\multicampus\\s03p23a409\\yolact\\" 
@@ -110,15 +106,9 @@ public class WebServercontroller {
 			 	try {
 			 		//yolact eval.py 파일 실행 
 			 		p = rt.exec(anaconda + " && " + pythonCommand);
-			 		// eval.py 뒤에 명령어..? 옵션 이거 설정을 어떻게 하징,, 
-			 		// 디텍션한 결과 가지고 와야 하는뎅 
-			 	//	int exitVal = p.exitValue();
-			 		
 			 		System.out.println(p);
-			 		System.out.println("나와랑ㅜㅜ");
-			 	//	List<MultipartFile> fileList = mtfRequest.getFiles(studentNumber);
-			 		//System.out.println(fileList);
-			 		
+			 		System.out.println("....");
+			 	
 			 	}catch (Exception e) {
 					e.printStackTrace();
 				}finally {
@@ -129,18 +119,100 @@ public class WebServercontroller {
 					while ((line = br.readLine()) != null) {
 						sb.append(line).append('\n');
 					}
-					showImages();
+					res = showImages();
 					System.out.println("> exit value: " + p.exitValue() + ", \n" + sb.toString());
 					p.destroy();
+					System.out.println(res.toString());
 				}
 	  
 	        } catch (IOException ioe) {
 	           System.out.println("살패으아으아ㅡ아ㅡ아ㅡㅏ");
 	            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 	        } 
-	        return ResponseEntity.ok().build();
+	        return res;
 	    }
 
+
+	public ResponseEntity<ImageUploadResponse> showImages () throws IOException {
+	    String boundary="---------THIS_IS_THE_BOUNDARY";
+	    File path = new File("C:\\Users\\multicampus\\s03p23a409\\yolact\\images\\output\\");
+	    File[] fileList= Arrays.stream(path.listFiles())
+	    		.filter(e -> e.getName().indexOf(String.valueOf(num)) != -1).toArray(File[]::new);
+	    String name = "";
+	    String filen = num+""; 
+	    String id = null;
+	    List<byte[]> imagesData = new ArrayList<byte[]>();
+	    for(int i = 0; i< fileList.length; i++) {
+	    	 imagesData.add(FileUtils.readFileToByteArray(fileList[i]));
+	    }
+	    
+	    List<ImageObject> objects = new ArrayList<ImageObject>();
+	    int idx=0;
+	    	for(File file : fileList) {
+	    		System.out.println(file.getName());
+	    		name = file.getName().split("\\.")[0];
+	    		
+	    		String [] arr = name.split("_");
+	    		
+	    		String type =arr[1];
+	    		if(!type.equals("mask")) {
+	    			id = arr[2];
+	    		} else {
+	    			id = "0";
+	    		}
+	    		// 이미지 파일 명 쪼개기 
+	    		ImageObject io = new ImageObject(file.getName(), type, Integer.parseInt(id),  imagesData.get(idx));
+	    		objects.add(io);
+	    		
+	    	
+	    		idx +=1;
+	    	}
+		 
+	  
+	    List<String> contentTypes = new ArrayList<>();
+	    for (int i = 0; i < fileList.length; i++) {
+	    	contentTypes.add(MediaType.IMAGE_PNG_VALUE);
+			
+		}
+	    byte[][] allImages = getMultipleImageResponse(boundary,fileList, contentTypes,imagesData);
+	 
+	    ImageUploadResponse ir = new ImageUploadResponse(num+"", objects);
+//	    final HttpHeaders headers = new HttpHeaders();
+//	    headers.set("Content-Type","multipart/x-mixed-replace; boundary=" + boundary);
+//	    System.out.println(ir.toString());
+	    
+	    return new ResponseEntity<>(ir,HttpStatus.CREATED);
+	}
+	
+	private static byte[] concat(byte[] a, byte[] b) {
+		byte[] r = Arrays.copyOf(a, a.length + b.length);
+		System.arraycopy(r, a.length, b, 0, b.length);
+		
+		return r;
+	}
+
+	private static byte[][] getMultipleImageResponse(String boundary, File[] fileList, List<String> contentTypes, List<byte[]> imagesData){
+		byte[][] finalByteArray = new byte[fileList.length][0];
+	    Integer imagesCounter = -1;
+	    for(File imageName : fileList){
+	        imagesCounter++;
+	      
+	        byte[] currentImageByteArray = Base64.encodeBase64(imagesData.get(imagesCounter));
+//	        finalByteArray = concat(finalByteArray, concat(currentImageByteArray, "\r\n\r\n".getBytes()));
+//	        if (imagesCounter == fileList.length - 1) {
+//	            String end = "--" + boundary + "--";
+//	            finalByteArray = concat(finalByteArray, end.getBytes());
+//	        }
+	        
+	        finalByteArray[imagesCounter] = currentImageByteArray;
+	    }
+	    return finalByteArray;
+	}
+	private static byte[] getMultipleImageResponse(byte[] imagesData){
+	  return Base64.encodeBase64(imagesData);
+
+	}
+	
 	
 	private ResponseEntity<Map<String, Object>> handleSuccess(Object data) {
 		Map<String, Object> resultMap = new HashMap<>();
@@ -155,73 +227,6 @@ public class WebServercontroller {
 		resultMap.put("data", e.getMessage());
 		return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
-	
-	public ResponseEntity<byte[]> showImages () throws IOException {
-	    String boundary="---------THIS_IS_THE_BOUNDARY";
-	    File path = new File("C:\\Users\\multicampus\\s03p23a409\\yolact\\images\\output\\");
-	    File fileList[] = path.listFiles();
-	    String name = "";
-	    String filen = num+""; 
-
-//	    if(fileList.length > 0){
-//	    	for(File file : fileList) {
-//	    		name = file.getName();
-//	    		if(name.contains(filen)) // 이거 if 왜 있죠
-//	    		{
-////	    			System.out.println(name);
-//	    
-//	    		}
-//	    		
-//	    	}
-//	    }
-	  
-	    //List<String> imageNames = Arrays.asList(new String[]{"C:\\Users\\multicampus\\s03p23a409\\yolact\\images\\output\\"+filename});
-	    
-	    List<String> contentTypes = new ArrayList<>();
-	    for (int i = 0; i < fileList.length; i++) {
-	    	contentTypes.add(MediaType.IMAGE_PNG_VALUE);
-			
-		}
-	    List<byte[]> imagesData = new ArrayList<byte[]>();
-	    for(int i = 0; i< fileList.length; i++) {
-	    	 imagesData.add(FileUtils.readFileToByteArray(fileList[i]));
-	    }
-	    byte[] allImages = getMultipleImageResponse(boundary,fileList, contentTypes,imagesData);
-	    final HttpHeaders headers = new HttpHeaders();
-	    headers.set("Content-Type","multipart/x-mixed-replace; boundary=" + boundary);
-	    return new ResponseEntity<byte[]>(allImages,headers,HttpStatus.CREATED);
-	}
-	
-	private static byte[] concat(byte[] a, byte[] b) {
-		byte[] r = Arrays.copyOf(a, a.length + b.length);
-		System.arraycopy(r, a.length, b, 0, b.length);
-		
-		return r;
-	}
-
-	private static byte[] getMultipleImageResponse(String boundary, File[] fileList, List<String> contentTypes, List<byte[]> imagesData){
-	    System.out.println(fileList.length);
-	    System.out.println(contentTypes.toString());
-	    System.out.println(imagesData.size());
-		byte[] finalByteArray = new byte[0];
-	    Integer imagesCounter = -1;
-	    for(File imageName : fileList){
-	        imagesCounter++;
-	        String header="--" + boundary 
-	                + "\r\nContent-Disposition: form-data; name=\"" + imageName
-	                + "\"; filename=\"" + imageName + "\"\r\n"
-	                + "Content-type: " + contentTypes.get(imagesCounter) + "\r\n\r\n";
-	        byte[] currentImageByteArray = concat(header.getBytes(), imagesData.get(imagesCounter));
-	        finalByteArray = concat(finalByteArray, concat(currentImageByteArray, "\r\n\r\n".getBytes()));
-	        if (imagesCounter == fileList.length - 1) {
-	            String end = "--" + boundary + "--";
-	            finalByteArray = concat(finalByteArray, end.getBytes());
-	        }
-	    }
-	    return finalByteArray;
-	}
-	
 	
 	
 
